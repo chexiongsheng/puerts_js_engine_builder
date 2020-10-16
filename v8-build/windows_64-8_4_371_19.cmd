4400fcbe8b53df8a709b105b92b0f593d7d57276
johnche <johnche@tencent.com>
2020-10-16 19:22:06 +0800

    window v8 args.gn

--- v8-build/windows_64-8_4_371_19.cmd
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -25,7 +25,8 @@ call gclient sync
 
 
 echo =====[ Building V8 ]=====
-call python .\tools\dev\v8gen.py x64.release -vv -- target_cpu="""x64""" v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false
+call python .\tools\dev\v8gen.py x64.release
+copy ..\..\v8-build\args_win64_8_4_371_19.gn .\out.gn\x64.release\args.gn
 
 call ninja -C out.gn\x64.release -t clean
 call ninja -C out.gn\x64.release v8
