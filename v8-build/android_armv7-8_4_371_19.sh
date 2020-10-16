5758a1e1debe12d4251fea86c4b6490548315f15
johnche <johnche@tencent.com>
2020-10-16 17:50:10 +0800

    first commit

---
+++ v8-build/android_armv7-8_4_371_19.sh
@@ -0,0 +1,57 @@
+sudo apt-get install -y \
+    pkg-config \
+    git \
+    subversion \
+    curl \
+    wget \
+    build-essential \
+    python \
+    xz-utils \
+    zip
+
+git config --global user.name "johnche"
+git config --global user.email "johnche@tencent.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['android']" >> .gclient
+cd ~/v8/v8
+./build/install-build-deps-android.sh
+git checkout refs/tags/8.4.371.19
+gclient sync
+
+
+echo "=====[ Building V8 ]====="
+python ./tools/dev/v8gen.py arm.release -vv -- '
+target_os = "android"
+target_cpu = "arm"
+is_debug = false
+v8_enable_i18n_support= false
+v8_target_cpu = "arm"
+use_goma = false
+v8_use_snapshot = true
+v8_use_external_startup_data = true
+v8_static_library = true
+strip_absolute_paths_from_debug_symbols = false
+strip_debug_info = false
+symbol_level=1
+use_custom_libcxx=false
+use_custom_libcxx_for_host=true
+'
+ninja -C out.gn/arm.release -t clean
+ninja -C out.gn/arm.release v8_libplatform
+ninja -C out.gn/arm.release v8
---
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -0,0 +1,31 @@
+git config --global user.name "johnche"
+git config --global user.email "johnche@tencent.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+cd %HOMEPATH%
+echo =====[ Getting Depot Tools ]=====
+powershell -command "Invoke-WebRequest https://storage.googleapis.com/chrome-infra/depot_tools.zip -O depot_tools.zip"
+7z x depot_tools.zip -o*
+set PATH=%CD%\depot_tools;%PATH%
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+call gclient
+
+
+mkdir v8
+cd v8
+
+echo =====[ Fetching V8 ]=====
+call fetch v8
+echo target_os = ['win'] >> .gclient
+cd v8
+call git checkout refs/tags/8.4.371.19
+call gclient sync
+
+
+echo =====[ Building V8 ]=====
+call python .\tools\dev\v8gen.py x64.release -vv -- target_os="""win""" v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false
+
+call ninja -C out.gn\x64.release -t clean
+call ninja -C out.gn\x64.release v8
