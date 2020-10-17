1cc580e2af89675b760814b53d0af7321ec8adae
johnche <johnche@tencent.com>
2020-10-17 13:14:30 +0800

    window用gn命令生成工程，并切换环境到vs2017

--- v8-build/windows_64-8_4_371_19.cmd
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -20,12 +20,12 @@ echo =====[ Fetching V8 ]=====
 call fetch v8
 echo target_os = ['win'] >> .gclient
 cd v8
-call git checkout 8.6.395.13
+call git checkout 8.4.371.19
 call gclient sync
 
 
 echo =====[ Building V8 ]=====
-call python .\tools\dev\v8gen.py x64.release -vv -- target_os="""win""" target_cpu="""x64""" v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false
+call gn gen out\x64.release -args='target_cpu = "x64" v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false'
 
-call ninja -C out.gn\x64.release -t clean
-call ninja -C out.gn\x64.release v8
+call ninja -C out\x64.release -t clean
+call ninja -C out\x64.release v8
