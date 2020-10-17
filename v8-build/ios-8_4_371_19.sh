2ccb033da62af1174a90809d5d321ffbb2e28c5f
johnche <johnche@tencent.com>
2020-10-17 14:55:48 +0800

    ios

---
+++ v8-build/ios-8_4_371_19.sh
@@ -0,0 +1,40 @@
+git config --global user.name "johnche"
+git config --global user.email "johnche@tencent.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['ios']" >> .gclient
+cd ~/v8/v8
+git checkout refs/tags/8.4.371.19
+gclient sync
+
+
+echo "=====[ Building V8 ]====="
+python ./tools/dev/v8gen.py x64.release -vv -- '
+target_os = "ios"
+enable_ios_bitcode = true
+ios_deployment_target = 10
+is_component_build = false
+is_debug = false
+use_custom_libcxx = false 
+use_xcode_clang = true
+v8_enable_i18n_support = false 
+v8_monolithic = true
+v8_use_external_startup_data = false
+'
+ninja -C out.gn/x64.release -t clean
+ninja -C out.gn/x64.release v8
