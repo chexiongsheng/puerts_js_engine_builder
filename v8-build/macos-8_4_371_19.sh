73d57e3d0067a489109deb023a3e2658e919350a
johnche <johnche@tencent.com>
2020-10-17 13:27:30 +0800

    macos

---
+++ v8-build/macos-8_4_371_19.sh
@@ -0,0 +1,39 @@
+git config --global user.name "johnche"
+git config --global user.email "johnche@tencent.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['mac']" >> .gclient
+cd ~/v8/v8
+git checkout refs/tags/8.4.371.19
+gclient sync
+
+
+echo "=====[ Building V8 ]====="
+python ./tools/dev/v8gen.py x64.release -vv -- '
+target_os = "mac"
+is_debug = false
+v8_enable_i18n_support= false
+v8_target_cpu = "arm64"
+v8_use_snapshot = true
+v8_use_external_startup_data = true
+v8_static_library = true
+strip_debug_info = false
+symbol_level=1
+'
+ninja -C out.gn/x64.release -t clean
+ninja -C out.gn/x64.release v8
--- v8-build/windows_64-8_4_371_19.cmd
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -25,7 +25,7 @@ call gclient sync
 
 
 echo =====[ Building V8 ]=====
-call gn gen out\x64.release -args='target_cpu = "x64" v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false'
+call gn gen out.gn\x64.release -args="v8_use_external_startup_data=true v8_enable_i18n_support=false is_debug=false v8_static_library=true is_clang=false strip_debug_info=true symbol_level=0 v8_enable_pointer_compression=false"
 
-call ninja -C out\x64.release -t clean
-call ninja -C out\x64.release v8
+call ninja -C out.gn\x64.release -t clean
+call ninja -C out.gn\x64.release v8
