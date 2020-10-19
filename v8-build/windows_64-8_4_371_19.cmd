83912456ca55d03a93f46bae2b76dadbe90fba76
V8 Windows Builder <v8.windows.builder@localhost>
2020-10-19 17:01:11 +0800

    refs/tags/8.4.371.19

--- v8-build/windows_64-8_4_371_19.cmd
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -13,8 +13,11 @@ cd v8
 echo =====[ Fetching V8 ]=====
 call fetch v8
 cd v8
-
-call git checkout 8.6.395.13
+call git checkout refs/tags/8.4.371.19
+cd test\test262\data
+call git config --system core.longpaths true
+call git restore *
+cd ..\..\..\
 call gclient sync
 
 
