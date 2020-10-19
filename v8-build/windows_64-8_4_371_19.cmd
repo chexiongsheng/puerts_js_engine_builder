8cb5d2e39bcdcad4d6c3f0ddc87f92858d0e2eb4
johnche <johnche@tencent.com>
2020-10-19 15:19:52 +0800

    ios用macos-latest

--- v8-build/windows_64-8_4_371_19.cmd
+++ v8-build/windows_64-8_4_371_19.cmd
@@ -15,12 +15,7 @@ call fetch v8
 cd v8
 
 call git checkout 8.6.395.13
-echo =====[ Sync 8.6.395.13 ]=====
 call gclient sync
-
-call git checkout 8.4.371.19
-echo =====[ Sync 8.4.371.19 ]=====
-call gclient sync --verbose
 
 
 echo =====[ Building V8 ]=====
